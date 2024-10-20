fbfef6e89d73c4089c4b5ac53fbb3dcfdfaf9740
iamdwn <truongdinhdongduong@gmail.com>
2024-10-21 04:33:31 +0700

    feat: add guess feature

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let hiddenWord = "tichluytuban";
+let displayWord = "_".repeat(hiddenWord.length).split('');
+
+document.getElementById('hiddenWord').textContent = displayWord.join(' ');
+
+function guessLetter() {
+    let guess = document.getElementById('guessInput').value.toLowerCase();
+    let message = document.getElementById('message');
+    let found = false;
+
+    for (let i = 0; i < hiddenWord.length; i++) {
+        if (hiddenWord[i] === guess) {
+            displayWord[i] = guess;
+            found = true;
+        }
+    }
+
+    document.getElementById('hiddenWord').textContent = displayWord.join(' ');
+
+    if (found) {
+        message.textContent = `Chữ "${guess}" đúng!`;
+    } else {
+        message.textContent = `Chữ "${guess}" không có trong từ.`;
+    }
+
+    if (!displayWord.includes('_')) {
+        message.textContent = 'Chúc mừng, bạn đã đoán đúng từ!';
+    }
+
+    document.getElementById('guessInput').value = '';
+}
